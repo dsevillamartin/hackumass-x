75d3202c36c462e3c2063f7e065bf7511db6538a
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2023-12-07 19:55:52 -0500

    Update API endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import { Client, Account, ID, Databases } from 'appwrite'
 
 const client = new Client()
-  .setEndpoint('https://api.blahgenda.tech/v1') // Your API Endpoint
+  .setEndpoint('https://api-blahgenda.dsev.dev/v1') // Your API Endpoint
   .setProject('636f502bd6d75a45b48b') // Your project ID
 
 export const account = new Account(client)
